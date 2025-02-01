78e155c6a62212da584803a54e90affd17507498
Can Çevik <mazlumcancevik@gmail.com>
2025-02-01 22:05:36 +0300

    fix: export types directly from sdk

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,6 @@
 export * from "./decorators/inject-infisical.decorator";
 export * from "./infisical.module";
 export * from "./interfaces/infisical-options.interface";
+
 export { InfisicalSDK, DynamicSecretProviders } from "@infisical/sdk";
-export {
-  ApiV1DashboardSecretsOverviewGet200ResponseSecretsInner,
-  ApiV1DashboardSecretsOverviewGet200ResponseSecretsInnerTagsInner,
-  ApiV1DynamicSecretsDelete200Response,
-  ApiV1DynamicSecretsGet200ResponseDynamicSecretsInner,
-  ApiV1DynamicSecretsLeasesLeaseIdDelete200Response,
-  ApiV1DynamicSecretsLeasesPost200Response,
-  ApiV1DynamicSecretsNameLeasesGet200ResponseLeasesInner,
-  ApiV1DynamicSecretsPost200Response,
-  ApiV1SecretApprovalRequestsIdGet200ResponseApprovalCommitsInnerSecretMetadataInner,
-  ApiV1SecretApprovalRequestsIdMergePost200ResponseApproval,
-  ApiV1SecretSecretIdSecretVersionsGet200ResponseSecretVersionsInner,
-  ApiV3SecretsRawGet200Response,
-  ApiV3SecretsRawGet200ResponseImportsInner,
-  ApiV3SecretsRawSecretNameGet200Response,
-  ApiV3SecretsRawSecretNameGet200ResponseSecret,
-  ApiV3SecretsRawSecretNamePost200Response,
-} from "./interfaces/api-return.interface";
+export type * from "@infisical/sdk";
